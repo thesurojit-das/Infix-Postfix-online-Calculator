4ede533f982845106392428f8dbb9131eb333d88
Surojit <81943886+thesurojit-das@users.noreply.github.com>
2022-06-20 18:30:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express=require('express')
 const bodyParser = require('body-parser');
 const app=express();
-const port=7000;
 var path = require('path') //to get css file
 app.use(express.static(path.join(__dirname, 'public'))); //to get css file
 app.use(bodyParser.urlencoded({ extended: true })); 
-
+const port =process.env.PORT||7000;
 app.listen(port,()=>{
     console.log(`Example app listening on port ${port}`)
 });
